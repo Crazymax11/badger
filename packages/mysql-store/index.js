f511fe6c4d756a7517003d9d097d99c37b5b90e0
Max Sosnov <crazymax101@gmail.com>
2017-10-31 19:55:25 +0700

    Apply store tests to mysql store

--- packages/mysql-store/index.js
+++ packages/mysql-store/index.js
@@ -16,15 +16,16 @@ type Password = string;
 type Database = string;
 type MysqlQuery = string;
 
-class MysqlStore implements Store {
+export class RawMysqlStore implements Store {
   pool: Pool;
-  constructor(host: Host, user: User, password: Password, database: Database) {
-    this.pool = mysql.createPool({
-      connectionLimit: 10,
-      host,
-      user,
-      password,
-      database
+  constructor(pool: Pool) {
+    this.pool = pool;
+    this.checkDb().then(isConfigured => {
+      if (!isConfigured) {
+        return this.createDb();
+      }
+
+      return true;
     });
   }
   checkDb(): Promise<boolean> {
@@ -63,6 +64,21 @@ class MysqlStore implements Store {
     time: Time
   ): Promise<StoreReturn> {
     return new Promise((resolve, reject) => {
+      if (typeof time !== 'number') {
+        throw new Error('time must be a number');
+      }
+
+      if (typeof status !== 'string') {
+        throw new Error('status must be a string');
+      }
+
+      if (typeof project !== 'string') {
+        throw new Error('project must be a string');
+      }
+
+      if (typeof subject !== 'string') {
+        throw new Error('subject must be a string');
+      }
       const query = storeToMysql(project, subject, status, time);
       this.pool.query(query, err => {
         if (err) {
@@ -78,27 +94,32 @@ class MysqlStore implements Store {
   }
 
   getLast(project: Project, subject: Subject): Promise<StoreReturn> {
-    return new Promise((resolve, reject) => {
-      this.pool.query(
-        'SELECT status FROM badges WHERE project = ? AND subject = ? LIMIT 1',
-        (err, result) => {
-          if (err) {
-            return reject(err);
-          }
+    if (typeof project !== 'string') {
+      throw new Error('project must be a string');
+    }
 
-          if (!result.length) {
-            // TODO сделать обработку ненверных данных баджами
-            return resolve({
-              status: 'none',
-              subject
-            });
-          }
+    if (typeof subject !== 'string') {
+      throw new Error('subject must be a string');
+    }
+    return new Promise((resolve, reject) => {
+      const query = getLastFromMysql(project, subject);
+      this.pool.query(query, (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+
+        if (!result.length) {
           return resolve({
-            status: result[0].status,
+            status: 'none',
             subject
           });
         }
-      );
+
+        return resolve({
+          status: result[0].status,
+          subject
+        });
+      });
     });
   }
 }
@@ -137,6 +158,19 @@ export function createTable(): MysqlQuery {
         status VARCHAR(50) NOT NULL,
         time TIMESTAMP
     )`;
+}
+
+class MysqlStore extends RawMysqlStore implements Store {
+  constructor(host: Host, user: User, password: Password, database: Database) {
+    const pool = mysql.createPool({
+      connectionLimit: 10,
+      host,
+      user,
+      password,
+      database
+    });
+    super(pool);
+  }
 }
 
 export default MysqlStore;
