d72e34a71693e54e21dfdd380628b8cda42276a6
Max Sosnov <crazymax101@gmail.com>
2017-11-14 18:17:27 +0700

    Add git badger generator

---
+++ packages/generator-git-badger-badge/generators/app/index.js
@@ -0,0 +1,38 @@
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+    prompting() {
+        return this.prompt([{
+            type: 'input',
+            name: 'badgeName',
+            message: 'enter your badge name (kebab case prefered, ex: eslint-erros)',
+            store: true
+        }, {
+            type: 'input',
+            name: 'badgeDescription',
+            message: 'enter your badge description',
+            store: true
+        }]).then(answers => {
+            this.badgeName = answers.badgeName;
+            this.badgeDescription = answers.badgeDescription;
+        })
+    }
+    writing() {
+        this.fs.copyTpl(
+            this.templatePath('index.js'),
+            this.destinationPath('index.js'),
+            {
+                badgeName: this.badgeName,
+                badgeDescription: this.badgeDescription
+            }
+        )
+        this.fs.copyTpl(
+            this.templatePath('_package.json'),
+            this.destinationPath('package.json'),
+            {
+                badgeName: this.badgeName,
+                badgeDescription: this.badgeDescription
+            }
+        )
+    }
+}
