243d8723cecdf6485ed7c05751f9c5da914934e9
msosnov <crazymax101@gmail.com>
2017-12-11 22:15:45 +0700

    :sparkles: (header) Make header reactive

--- packages/git-badger-app/web/components/nav-header.vue
+++ packages/git-badger-app/web/components/nav-header.vue
@@ -1,23 +1,32 @@
 <template>
-    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-        <el-menu-item index="1">Here will be a logo</el-menu-item>
-        <el-menu-item index="2"><a href="/badges" target="_blank">Badges</a></el-menu-item>
-        <el-menu-item index="3"><a href="/projects" target="_blank">Projects</a></el-menu-item>
-        <el-menu-item index="4"><a href="/about" target="_blank">About</a></el-menu-item>
+    <el-menu :default-active="defaultActive" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="0">Here will be a logo</el-menu-item>
+        <el-menu-item index="1"><a href="/badges" target="_blank">Badges</a></el-menu-item>
+        <el-menu-item index="2"><a href="/projects" target="_blank">Projects</a></el-menu-item>
+        <el-menu-item index="3"><a href="/about" target="_blank">About</a></el-menu-item>
     </el-menu>
 </template>
 <<script>
+const map = [
+    '/',
+    '/badges',
+    '/projects',
+    '/about'
+]
 export default {
   data() {
       return {
-        activeIndex: '1'
+        activeIndex: '0'
       };
     },
     methods: {
       handleSelect(key, keyPath) {
-        console.log(key, keyPath);
-        this.activeIndex = key;
+        window.location = map[key];
       }
+    },
+    created() {
+        this.defaultActive = String(map.indexOf(location.pathname));
+
     }
 }
 </script>
